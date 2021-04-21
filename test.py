3151c9e7aeaa8bf3a4095ac875df1668ab0c82a0
Weslia <tstststt@yandex.ru>
2021-04-21 20:40:17 +0300

    test 5

--- test.py
+++ test.py
@@ -3,3 +3,7 @@ from requests import get
 print(get('http://127.0.0.1:5000/api/items').json())
 
 print(get('http://127.0.0.1:5000/api/items/2').json())
+
+print(get('http://127.0.0.1:5000/api/items/9999999').json())
+
+print(get('http://127.0.0.1:5000/api/items/safa').json())
